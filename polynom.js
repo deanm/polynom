623700275a4b61cfd3ba746b388a73f731922cc6
Dean McNamee <dean@gmail.com>
2015-01-08 15:15:09 +0100

    Clean up Rational/Polynomial methods, pull out to prototype.
    This should be a better approach, because it means there doesn't
    need to be a closure for every call to the constructor,
    and all of the methods are created once and shared on the prototype.
    
    Additionally clean up lots of things and add tests.
    
    Follow a Z/Q/R convention for argument types.

--- polynom.js
+++ polynom.js
@@ -1,5 +1,12 @@
 // (c) Dean McNamee <dean@gmail.com>, 2014.
 
+// Some abbreviations used, often as function name suffixes:
+//   Q - Rationals (set ℚ), object of class Rational
+//   Z - Integers (set ℤ), JavaScript number
+//   R - Reals (set ℝ), JavaScript number
+//   P - Polynomial, object of class Polynomial
+//   $ - Operation is mutable, happens in place without creating a new object.
+
 function binary_gcd(u, v) {
   if (u === v) return u;
   if (u === 0) return v;
@@ -48,9 +55,11 @@ function bino(n, k) {
 }
 
 function Rational(num, denom) {
-  var s = 1;
+  if (denom === 0) throw "Invalid Rational, zero denominator.";
 
-  if (num === 0) denom = 1;
+  if (num === 0) denom = 1;  // Normalize all zeros to the same 0/1.
+
+  var s = 1;
 
   if (num < 0) {
     s = -s;
@@ -71,91 +80,93 @@ function Rational(num, denom) {
     }
   }
 
+  // TODO: Better to switch to p/q mathematical convention?
   this.n = num * s;
   this.d = denom;
-
-  this.toNumber = function() { return this.n / this.d; };
-  this.toString = function() {
-    return this.d === 1 ? this.n+'' : this.n + '/' + this.d;
-  };
-
-  this.toUnicode = function() {
-    if (this.n === 1) {
-      if (this.d === 2) return "\u00bd";
-      if (this.d === 4) return "\u00bc";
-    }
-    return this.toString();
-  };
-
-  this.divScalar = function(v) {
-    return new Rational(this.n, this.d * v);
-  };
-
-  this.mulScalar = function(v) {
-    return new Rational(this.n * v, this.d);
-  };
-
-  this.divScalar = function(v) {
-    return new Rational(this.n, this.d * v);
-  };
-
-  this.mul = function(r) {
-    return new Rational(this.n * r.n, this.d * r.d);
-  };
-
-  this.div = function(r) {
-    return new Rational(this.n * r.d, this.d * r.n);
-  };
-
-  this.add$ = function(r) {
-    var c = this.add(r);
-    this.n = c.n; this.d = c.d;
-    return this;
-  };
-
-  this.add = function(r) {
-    return this.d === r.d ?
-        new Rational(this.n + r.n, this.d) :
-        new Rational(this.n * r.d + r.n * this.d, this.d * r.d);
-  };
-
-  this.sub = function(r) {
-    return new Rational(this.n * r.d - r.n * this.d, this.d * r.d);
-  };
-
-  this.neg = function() {
-    return new Rational(-this.n, this.d);
-  };
-
-  this.recip = function() {
-    return new Rational(this.d, this.n);
-  };
-
-  this.isOne = function() {
-    return this.n === 1 && this.d === 1;
-  };
-
-  this.isOneOrNegOne = function() {
-    return (this.n === 1 || this.n === -1) && this.d === 1;
-  };
-
-  this.isZero = function() {
-    return this.n === 0;
-  };
-
-  this.isNeg = function() {
-    return (this.n < 0) ^ (this.d < 0);
-  };
-
-  this.dup = function() {
-    return new Rational(this.n, this.d);
-  };
 }
 
-Rational.createFromInteger = function(n) {
+/* static */
+Rational.createFromZ = function(n) {
   if (n !== (n|0)) { console.trace('xx'); throw 'xx'; }
   return new Rational(n, 1);
-}
+};
+
+Rational.prototype.isOne = function() {
+  return this.n === 1 && this.d === 1;
+};
+
+Rational.prototype.isOneOrNegOne = function() {
+  return (this.n === 1 || this.n === -1) && this.d === 1;
+};
+
+Rational.prototype.isZero = function() {
+  return this.n === 0;
+};
+
+Rational.prototype.isNeg = function() {
+  return !!((this.n < 0) ^ (this.d < 0));
+};
+
+Rational.prototype.divZ = function(v) {
+  return new Rational(this.n, this.d * v);
+};
+
+Rational.prototype.mulZ = function(v) {
+  return new Rational(this.n * v, this.d);
+};
+
+Rational.prototype.divQ = function(r) {
+  return new Rational(this.n * r.d, this.d * r.n);
+};
+
+Rational.prototype.mulQ = function(r) {
+  return new Rational(this.n * r.n, this.d * r.d);
+};
+
+Rational.prototype.addQ = function(r) {
+  return this.d === r.d ?
+      new Rational(this.n + r.n, this.d) :
+      new Rational(this.n * r.d + r.n * this.d, this.d * r.d);
+};
+
+Rational.prototype.addQ$ = function(r) {
+  var c = this.addQ(r);
+  this.n = c.n; this.d = c.d;
+  return this;
+};
+
+Rational.prototype.subQ = function(r) {
+  return new Rational(this.n * r.d - r.n * this.d, this.d * r.d);
+};
+
+Rational.prototype.neg = function() {
+  return new Rational(-this.n, this.d);
+};
+
+Rational.prototype.recip = function() {
+  return new Rational(this.d, this.n);
+};
+
+Rational.prototype.dup = function() {
+  return new Rational(this.n, this.d);
+};
+
+Rational.prototype.toR = function() {
+  return this.n / this.d;
+};
+
+Rational.prototype.toString = function() {
+  return this.n + (this.d === 1 ? '' : '/' + this.d);
+};
+
+Rational.prototype.toUnicode = function() {
+  if (this.n === 1) {
+    if (this.d === 2) return "\u00bd";
+    if (this.d === 4) return "\u00bc";
+  }
+  return this.toString();
+};
+
 
 function Polynomial(degree, coef) {
   this.degree = degree;
@@ -175,213 +186,224 @@ function Polynomial(degree, coef) {
     throw 'Coefficients array incorrect length for degree.';
 
   this.c = coef;
-
-  this.integrate = function() {
-    var res = new Polynomial(degree + 1);
-    res[0] = new Rational(0, 1);
-    for (var i = 0, il = coef.length; i < il; ++i) {
-      var r = coef[i];
-      res.c[i+1] = r.divScalar(i+1);
-    }
-    return res;
-  }
-
-  this.diff = function() {
-    if (degree === 0)
-      return new Polynomial(0, [new Rational(0, 1)]);
-
-    var res = new Polynomial(degree - 1);
-    for (var i = 1, il = coef.length; i < il; ++i) {
-      var r = coef[i];
-      res.c[i-1] = r.mulScalar(i);
-    }
-    return res;
-  }
-
-  this.add = function(p) {
-    var d = this.degree > p.degree ? this.degree : p.degree;
-    var n = new Polynomial(d);
-    for (var i = 0; i <= d; ++i) {
-      if (i > this.degree) {
-        n.c[i] = p.c[i];
-      } else if (i > p.degree) {
-        n.c[i] = coef[i];
-      } else {
-        n.c[i] = p.c[i].add(coef[i]);
-      }
-    }
-    return n;
-  };
-
-  this.mul = function(p) {  // super O(n^2).
-    var a = this.c;
-    var b = p.c;
-
-    var c = new Array(this.degree + p.degree + 1);
-    for (var i = 0, il = c.length; i < il; ++i)
-      c[i] = new Rational(0, 1);
-
-    for (var i = 0, il = a.length; i < il; ++i) {
-      for (var j = 0, jl = b.length; j < jl; ++j) {
-        c[i+j] = c[i+j].add(a[i].mul(b[j]));
-      }
-    }
-
-    while (c.length !== 1 && c[c.length - 1].isZero()) c.pop();
-
-    return new Polynomial(c.length - 1, c);
-  };
-
-  this.dup = function() {
-    var c = new Array(coef.length);
-    for (var i = 0, il = c.length; i < il; ++i) {
-      c[i] = coef[i].dup();
-    }
-    return new Polynomial(c.length - 1, c);
-  }
-
-  this.scaleScalar$ = function(s) {
-    for (var i = 0, il = coef.length; i < il; ++i) {
-      coef[i] = coef[i].mulScalar(s);
-    }
-    return this;
-  };
-
-  this.scaleScalar = function(s) {
-    var p = this.dup();
-    p.scaleScalar$(s);
-    return p;
-  };
-
-  this.scale$ = function(r) {
-    for (var i = 0, il = coef.length; i < il; ++i) {
-      coef[i] = coef[i].mul(r);
-    }
-    return this;
-  };
-
-  this.scale = function(r) {
-    var p = this.dup();
-    p.scale$(r);
-    return p;
-  };
-
-  this.subst = function(p) {  // composition better name?
-    var c = new Polynomial(0, [coef[0]]);
-    var pp = p;
-    for (var i = 1, il = coef.length; i < il; ++i) {
-      if (i !== 1) pp = pp.mul(p);  // Computationally brutal.
-      c = c.add(pp.scale(coef[i]));
-    }
-
-    return c;
-  };
-
-  this.pretty = function(sym) {
-    sym = sym === undefined ? 't' : sym;
-    var str = '';
-    for (var i = 0, il = coef.length; i < il; ++i) {
-      if (i === 0) {
-        str += coef[i];
-      } else if (i === 1) {
-        str += ' + ' + coef[i] + '*' + sym;
-      } else {
-        str += ' + ' + coef[i] + '*' + sym + '^' + i;
-      }
-    }
-    return str;
-  }
-
-  this.toJS = function(sym) {
-    sym = sym === undefined ? 't' : sym;
-    var str = '';
-    var ssym = sym;
-    var parts = [ ];
-    for (var i = 0, il = coef.length; i < il; ++i) {
-      if (i === 0) {
-        if (!coef[0].isZero())
-          parts.push(coef[0]);
-        continue;
-      }
-
-      if (coef[i].isZero()) {
-      } else if (coef[i].isOne()) {
-        parts.push(ssym);
-      } else {
-        parts.push(coef[i] + '*' + ssym);
-      }
-      ssym += '*' + sym;
-    }
-    if (parts.length === 0) parts = ['0'];
-    return parts.join(' + ');
-  }
-
-  this.toUnicode = function(sym) {
-    sym = sym === undefined ? 't' : sym;
-    var str = '';
-    var parts = [ ];
-    for (var i = 0, il = coef.length; i < il; ++i) {
-      if (coef[i].isZero())  // zero term
-        continue;
-
-      var up = '';
-      switch (i) {
-        case 0:
-          parts.push(coef[0].toUnicode());
-          continue;
-        case 1: break;
-        //case 1: up = "\u00b9"; break;
-        /*
-        case 2: up = "\u00b2"; break;
-        case 3: up = "\u00b3"; break;
-        case 4: up = "\u2074"; break;
-        case 5: up = "\u2075"; break;
-        case 6: up = "\u2076"; break;
-        case 7: up = "\u2077"; break;
-        case 8: up = "\u2078"; break;
-        case 9: up = "\u2079"; break;
-        */
-        default:
-          var up = '^' + i;
-          break;
-      }
-
-      if (coef[i].isOne()) {
-        parts.push(sym + up);
-      } else {
-        parts.push(coef[i].toUnicode() + '*' + sym + up);
-      }
-    }
-    return parts.join(' + ').replace(/ \+ \-/g, ' - ').replace(/ 1\*/g, ' ');
-  }
-
-  this.eval = function(t) {
-    var res = coef[0].toNumber();
-    var tt = t;
-    for (var i = 1, il = coef.length; i < il; ++i) {
-      res += tt * coef[i].toNumber();
-      tt *= t;
-    }
-    return res;
-  };
-
-  this.evalRational = function(t) {
-    var res = coef[0];
-    var tt = t;
-    for (var i = 1, il = coef.length; i < il; ++i) {
-      res = res.add(tt.mul(coef[i]));
-      tt = tt.mul(t);
-    }
-    return res;
-  };
 }
 
-Polynomial.createFromRational = function(r) {
+Polynomial.createFromQ = function(r) {
   return new Polynomial(0, [r]);
 };
 
-Polynomial.createFromInteger = function(v) {
-  return new Polynomial(0, [Rational.createFromInteger(v)]);
+Polynomial.createFromZ = function(v) {
+  return new Polynomial(0, [Rational.createFromZ(v)]);
+};
+
+Polynomial.prototype.mulZ$ = function(s) {
+  var c = this.c;
+  for (var i = 0, il = c.length; i < il; ++i) {
+    c[i] = c[i].mulZ(s);
+  }
+  return this;
+};
+
+Polynomial.prototype.mulZ = function(s) {
+  var p = this.dup();
+  p.mulZ$(s);
+  return p;
+};
+
+Polynomial.prototype.mulQ$ = function(r) {
+  var c = this.c;
+  for (var i = 0, il = c.length; i < il; ++i) {
+    c[i] = c[i].mulQ(r);
+  }
+  return this;
+};
+
+Polynomial.prototype.mulQ = function(r) {
+  var p = this.dup();
+  p.mulQ$(r);
+  return p;
+};
+
+Polynomial.prototype.addP = function(p) {
+  var d = this.degree > p.degree ? this.degree : p.degree;
+  var n = new Polynomial(d);
+  var c = this.c;
+  for (var i = 0; i <= d; ++i) {
+    if (i > this.degree) {
+      n.c[i] = p.c[i];
+    } else if (i > p.degree) {
+      n.c[i] = c[i];
+    } else {
+      n.c[i] = p.c[i].addQ(c[i]);
+    }
+  }
+  return n;
+};
+
+Polynomial.prototype.mulP = function(p) {  // super O(n^2).
+  var a = this.c;
+  var b = p.c;
+
+  var c = new Array(this.degree + p.degree + 1);
+  for (var i = 0, il = c.length; i < il; ++i)
+    c[i] = new Rational(0, 1);
+
+  for (var i = 0, il = a.length; i < il; ++i) {
+    for (var j = 0, jl = b.length; j < jl; ++j) {
+      c[i+j] = c[i+j].addQ(a[i].mulQ(b[j]));
+    }
+  }
+
+  while (c.length !== 1 && c[c.length - 1].isZero()) c.pop();
+
+  return new Polynomial(c.length - 1, c);
+};
+
+Polynomial.prototype.integrate = function() {
+  var res = new Polynomial(this.degree + 1);
+  res[0] = new Rational(0, 1);
+  var c = this.c;
+  for (var i = 0, il = c.length; i < il; ++i) {
+    var r = c[i];
+    res.c[i+1] = r.divZ(i+1);
+  }
+  return res;
+};
+
+Polynomial.prototype.diff = function() {
+  if (this.degree === 0)
+    return new Polynomial(0, [new Rational(0, 1)]);
+
+  var res = new Polynomial(this.degree - 1);
+  var c = this.c;
+  for (var i = 1, il = c.length; i < il; ++i) {
+    var r = c[i];
+    res.c[i-1] = r.mulZ(i);
+  }
+  return res;
+};
+
+Polynomial.prototype.dup = function() {
+  var c = new Array(this.c.length);
+  for (var i = 0, il = c.length; i < il; ++i) {
+    c[i] = this.c[i].dup();
+  }
+  return new Polynomial(c.length - 1, c);
+};
+
+Polynomial.prototype.substP = function(p) {  // composition better name?
+  var c = new Polynomial(0, [this.c[0]]);
+  var pp = p;
+  for (var i = 1, il = this.c.length; i < il; ++i) {
+    if (i !== 1) pp = pp.mulP(p);  // Computationally brutal.
+    c = c.addP(pp.mulQ(this.c[i]));
+  }
+
+  return c;
+};
+
+Polynomial.prototype.pretty = function(sym) {
+  sym = sym === undefined ? 't' : sym;
+  var str = '';
+  var c = this.c;
+  for (var i = 0, il = c.length; i < il; ++i) {
+    if (i === 0) {
+      str += c[i];
+    } else if (i === 1) {
+      str += ' + ' + c[i] + '*' + sym;
+    } else {
+      str += ' + ' + c[i] + '*' + sym + '^' + i;
+    }
+  }
+  return str;
+};
+
+////////
+
+Polynomial.prototype.toJS = function(sym) {
+  sym = sym === undefined ? 't' : sym;
+  var str = '';
+  var ssym = sym;
+  var parts = [ ];
+  var c = this.c;
+  for (var i = 0, il = c.length; i < il; ++i) {
+    if (i === 0) {
+      if (!c[0].isZero())
+        parts.push(c[0]);
+      continue;
+    }
+
+    if (c[i].isZero()) {
+    } else if (c[i].isOne()) {
+      parts.push(ssym);
+    } else {
+      parts.push(c[i] + '*' + ssym);
+    }
+    ssym += '*' + sym;
+  }
+  if (parts.length === 0) parts = ['0'];
+  return parts.join(' + ');
+};
+
+Polynomial.prototype.toUnicode = function(sym) {
+  sym = sym === undefined ? 't' : sym;
+  var str = '';
+  var parts = [ ];
+  for (var i = 0, il = coef.length; i < il; ++i) {
+    if (coef[i].isZero())  // zero term
+      continue;
+
+    var up = '';
+    switch (i) {
+      case 0:
+        parts.push(coef[0].toUnicode());
+        continue;
+      case 1: break;
+      //case 1: up = "\u00b9"; break;
+      /*
+      case 2: up = "\u00b2"; break;
+      case 3: up = "\u00b3"; break;
+      case 4: up = "\u2074"; break;
+      case 5: up = "\u2075"; break;
+      case 6: up = "\u2076"; break;
+      case 7: up = "\u2077"; break;
+      case 8: up = "\u2078"; break;
+      case 9: up = "\u2079"; break;
+      */
+      default:
+        var up = '^' + i;
+        break;
+    }
+
+    if (coef[i].isOne()) {
+      parts.push(sym + up);
+    } else {
+      parts.push(coef[i].toUnicode() + '*' + sym + up);
+    }
+  }
+  return parts.join(' + ').replace(/ \+ \-/g, ' - ').replace(/ 1\*/g, ' ');
+};
+
+Polynomial.prototype.evalR = function(t) {
+  var c = this.c;
+  var res = c[0].toR();
+  var tt = t;
+  for (var i = 1, il = c.length; i < il; ++i) {
+    res += tt * c[i].toR();
+    tt *= t;
+  }
+  return res;
+};
+
+Polynomial.prototype.evalQ = function(t) {
+  var c = this.c;
+  var res = c[0];
+  var tt = t;
+  for (var i = 1, il = c.length; i < il; ++i) {
+    res = res.addQ(tt.mulQ(c[i]));
+    tt = tt.mulQ(t);
+  }
+  return res;
 };
 
 function isRational(r) {
--- tests.js
+++ tests.js
@@ -11,40 +11,117 @@ function assert_eq(a, b) {
   }
 }
 
-/*
-var p = new Polynomial(3, [1, 4, 3, 5]);
-var p1 = p.integrate();
-var p2 = p1.diff();
-//console.log(p.pretty('t'));
-console.log(p1.pretty('t'));
-console.log(p2.pretty('t'));
-//console.log(p1.add(p2).pretty('t'));
-//console.log(p1.toJS('t'));
+function assert_throws(estr, cb) {
+  try {
+    cb();
+  } catch(e) {
+    assert_eq(estr, e.toString());
+    return;
+  }
+  throw 'Expected an exception.';
+}
 
-var p3 = new Polynomial(2, [0, 1, 1]);
-var p4 = new Polynomial(2, [1, 1, 1]);
-//console.log(p3.mul(p4).pretty('t'));
-//console.log(p1.mul(p2).pretty('t'));
-console.log(p3.toJS());
-console.log(p4.toJS());
-console.log(p3.subst(p4).toJS());
-*/
-
+function Q(p, q) { return new pn.Rational(p, q); }
 
 function test_rational() {
-  var r0 = new pn.Rational(194481, 130321);
-  assert_eq("388962/130321", r0.add(r0).toString());
+
+  assert_throws("Invalid Rational, zero denominator.", function() {
+    new pn.Rational(0, 0);
+  });
+  assert_throws("Invalid Rational, zero denominator.", function() {
+    new pn.Rational(1, 0);
+  });
+
+  assert_eq(true,  Q(1, 1).isOne());
+  assert_eq(true,  Q(1, 1).isOneOrNegOne());
+  assert_eq(false, Q(1, 1).isZero());
+  assert_eq(false, Q(1, 1).isNeg());
+
+  assert_eq(false, Q(0, 1).isOne());
+  assert_eq(false, Q(0, 1).isOneOrNegOne());
+  assert_eq(true,  Q(0, 1).isZero());
+  assert_eq(false, Q(1, 1).isNeg());
+
+  assert_eq(false, Q(-1, 1).isOne());
+  assert_eq(true,  Q(-1, 1).isOneOrNegOne());
+  assert_eq(false, Q(-1, 1).isZero());
+  assert_eq(true,  Q(-1, 1).isNeg());
+
+  var r0 = Q(194481, 130321);
+  r0 = r0.addQ(r0);
+  assert_eq("388962/130321", r0.toString());
+  r0 = r0.divZ(2);
+  assert_eq("194481/130321", r0.toString());
+  r0 = r0.divQ(Q(1, 2));
+  assert_eq("388962/130321", r0.toString());
+  r0 = r0.mulQ(Q(1, 2));
+  assert_eq("194481/130321", r0.toString());
+  r0 = r0.mulZ(2);
+  assert_eq("388962/130321", r0.toString());
+  r0 = r0.addQ(Q(13, 57));
+  assert_eq("1256053/390963", r0.toString());
+  r0 = r0.subQ(Q(13, 57));
+  assert_eq("388962/130321", r0.toString());
+  r0 = r0.recip();
+  assert_eq("130321/388962", r0.toString());
+  r0 = r0.neg();
+  assert_eq("-130321/388962", r0.toString());
+  r0 = r0.neg();
+  assert_eq("130321/388962", r0.toString());
 }
 
 function test_polynomial() {
-  var p6 = new pn.Polynomial(3, [new pn.Rational(-9261, 6859),
-                                 new pn.Rational(26459, 6859),
-                                 new pn.Rational(-25200, 6859),
-                                 new pn.Rational(-8000, 6859)]);
-  var p7 = new pn.Polynomial(1, [new pn.Rational(-21, 19),
-                                 new pn.Rational(20, 19)]);
 
-  assert_eq("194481/130321 + -740859/130321*t + 1058380/130321*t^2 + -336000/130321*t^3 + -160000/130321*t^4", p6.mul(p7).pretty());
+  assert_throws("Coefficients array incorrect length for degree.", function() {
+    new pn.Polynomial(2, [Q(1, 2), Q(2, 3)]);
+  });
+  assert_throws("Coefficients array incorrect length for degree.", function() {
+    new pn.Polynomial(0, [Q(1, 2), Q(2, 3)]);
+  });
+
+  assert_eq("4", pn.Polynomial.createFromZ(4).pretty());
+  assert_eq("3/4", pn.Polynomial.createFromQ(Q(3, 4)).pretty());
+
+  var p0 = new pn.Polynomial(1, [Q(1, 2), Q(2, 3)]);
+  assert_eq("1/2 + 2/3*t", p0.pretty());
+  p0 = p0.mulZ(2);
+  assert_eq("1 + 4/3*t", p0.pretty());
+  p0 = p0.mulQ(Q(1, 2));
+  assert_eq("1/2 + 2/3*t", p0.pretty());
+
+  var p1 = new pn.Polynomial(2, [Q(1, 3), Q(1, 2), Q(5, 1)]);
+  assert_eq(p0.addP(p1).pretty(), p1.addP(p0).pretty());
+  var p2 = p1.addP(p0);
+  assert_eq("5/6 + 7/6*t + 5*t^2", p2.pretty());
+
+  assert_eq(p0.mulP(p2).pretty(), p2.mulP(p0).pretty());
+  var p3 = p0.mulP(p2);
+  assert_eq("5/12 + 41/36*t + 59/18*t^2 + 10/3*t^3", p3.pretty());
+
+  var p4 = p3.integrate();
+  assert_eq("0 + 5/12*t + 41/72*t^2 + 59/54*t^3 + 5/6*t^4", p4.pretty());
+
+  var p5 = p4.diff();
+  assert_eq("5/12 + 41/36*t + 59/18*t^2 + 10/3*t^3", p5.pretty());
+
+  var p6 = new pn.Polynomial(3, [Q(-9261, 6859),
+                                 Q(26459, 6859),
+                                 Q(-25200, 6859),
+                                 Q(-8000, 6859)]);
+  var p7 = new pn.Polynomial(1, [Q(-21, 19),
+                                 Q(20, 19)]);
+
+  assert_eq("194481/130321 + -740859/130321*t + 1058380/130321*t^2 + -336000/130321*t^3 + -160000/130321*t^4", p6.mulP(p7).pretty());
+
+  var p8 = new pn.Polynomial(2, [Q(2, 1), Q(2, 1), Q(1, 2)]);
+  var p9 = new pn.Polynomial(1, [Q(1, 1), Q(1, 1)]);
+  var p10 = p8.substP(p9);
+  assert_eq("9/2 + 3*t + 1/2*t^2", p10.pretty());
+
+  assert_eq("9/2 + 3*t + 1/2*t*t", p10.toJS());
+
+  assert_eq(18, p10.evalR(3));
+  assert_eq("162/25", p10.evalQ(Q(3, 5)).toString());
 }
 
 test_rational();
